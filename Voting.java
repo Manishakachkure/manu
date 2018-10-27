2832fcee8158075a92f54aac80c60ff0ea0ce588
Kailas82 <44490053+Kailas82@users.noreply.github.com>
2018-10-26 21:25:40 -0700

    Update Voting.java

--- Voting.java
+++ Voting.java
@@ -3,6 +3,11 @@
 public static void main(String args[])
 {
 int a=24;
+ if(a==0)
+ {
+  System.out.println("type right age");
+ }
+  
 if(a>=18)
 {
 System.out.println("you are illegable for voting");
